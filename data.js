31119715688f7194273ab304938beecb9288eee2
bloodearnest <bloodearnest@gmail.com>
2018-06-22 19:25:56 +0100

    update party to level 4

--- data.js
+++ data.js
@@ -5,18 +5,30 @@ const player_data = {
     attacks: [
       {
         name: "Toll the Dead (full hitpoints)",
-        save: '13 Wis',
+        save: '15 Wis',
         half: false,
         damage: {necrotic: "d8"},
       }, {
         name: "Toll the Dead (injured)",
-        save: '13 Wis',
+        save: '15 Wis',
         half: false,
         damage: {necrotic: "d12"},
+      }, {
+        name: "Spiritual Weapon",
+        tohit: 7,
+        damage: {force: "d8+5"},
+        conditions: {
+            'Level 3-4': {force: "d8"},
+            'Level 5-6': {force: "2d8"},
+        }
       }, {
         name: "Guiding Bolt",
         tohit: 5,
         damage: {radiant: "4d6"},
+        conditions: {
+            'Level 2': {radiant: "1d6"},
+            'Level 3': {radiant: "2d6"},
+        },
       }, {
         name: "Dagger",
         tohit: "3",
@@ -36,14 +48,16 @@ const player_data = {
         damage: {"slashing": "d6+3"},
         conditions: {
           "Level 1 Smite": {radiant: "2d8"},
-        }
+          "Level 2 Smite": {radiant: "4d8"},
+        },
       }, {
         name: "Oathbringer (offhand)",
         tohit: 5,
-        damage: {"slashing": "d6"},
+        damage: {"slashing": "d6+3"},
         conditions: {
           "Level 1 Smite": {radiant: "2d8"},
-        }
+          "Level 2 Smite": {radiant: "4d8"},
+        },
       }
     ],
   },
@@ -55,11 +69,23 @@ const player_data = {
         damage: {piercing: "d8+2"},
       }, {
         name: "Vicious Mockery",
-        save: '13 Wis',
+        save: '14 Wis',
         half: false,
         damage: {
           psychic: "d4",
           effect: "disadvantage on next attack roll",
+        },
+      }, {
+        name: "Dissonant Whispers",
+        save: '14 Wis',
+        half: true,
+        damage: {
+          psychic: "3d6",
+          effect: "must use reaction to move full speed away from you.",
+        },
+        conditions: {
+            'Level 2': {weapon: "1d6"},
+            'Level 3': {weapon: "2d6"},
         },
       }
     ]
@@ -68,28 +94,28 @@ const player_data = {
     attacks: [
       {
         name: "Battleaxe (one handed)",
-        tohit: 5,
+        tohit: 6,
         damage: {"slashing": "d8+3"},
         conditions: {
           "Raging": {weapon: "2"}
         },
       }, {
         name: "Battleaxe (two handed)",
-        tohit: 5,
+        tohit: 6,
         damage: {"slashing": "d10+3"},
         conditions: {
           "Raging": {weapon: "2"}
         },
       }, {
         name: "Chakram",
-        tohit: 5,
+        tohit: 6,
         damage: {"slashing": "d6+3"},
         conditions: {
           "Raging (melee)": {weapon: "2"}
         }
       }, {
         name: "Handaxe",
-        tohit: 5,
+        tohit: 6,
         damage: {"slashing": "d6+3"},
         conditions: {
           "Raging (melee)": {weapon: "2"}
@@ -122,12 +148,34 @@ const player_data = {
     attacks: [
       {
         name: "Longbow",
-        tohit: 7,
-        damage: {"piercing": "d8+3"},
+        tohit: 8,
+        damage: {"piercing": "d8+4"},
+      }, {
+        name: "Bolas",
+        tohit: "8",
+        damage: {bludgeoning: "d4+4"},
+        effect: "Large and smaller creatures are rendered prone. DC 10 Str, DC 15 Dex, or 5 slashing damage to escape. If you have advantage, and both hit, target is also restrained."
+      }, {
+        name: "Blowgun",
+        tohit: "8",
+        damage: {piercing: "1"},
+      }, {
+        name: "Net",
+        tohit: "8",
+        damage: {bludgeoning: "0"},
+        effect: "Target is restrained. DC 10 Str check or 5 slashing damage to escape. Can only make 1 attack per turn."
+      }, {
+        name: "Trident (1 handed)",
+        tohit: "3",
+        damage: {piercing: "d6+1"},
+      }, {
+        name: "Trident (2 handed)",
+        tohit: "3",
+        damage: {piercing: "d8+1"},
       }, {
         name: "Cat's Claw",
         tohit: "3",
-        damage: {piercing: "d4+ 1"},
+        damage: {piercing: "d4+1"},
       }
     ],
   },
