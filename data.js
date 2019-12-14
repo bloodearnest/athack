7edf51304d5cbac33b9104a3a366f738927fb669
Simon Davy <bloodearnest@gmail.com>
2019-12-14 13:32:19 +0000

    Update to level 5

--- data.js
+++ data.js
@@ -5,8 +5,8 @@ const party_data = {
     attacks: [
       {
         name: "Firebolt",
-        tohit: 6,
-        damage: {"fire": "1d10"},
+        tohit: 7,
+        damage: {"fire": "2d10"},
         sounds: SPELL_SOUNDS,
       }, {
         name: "Shortsword",
@@ -14,13 +14,13 @@ const party_data = {
         damage: {"piercing": "d6+3"},
       }, {
         name: "Shocking Grasp",
-        tohit: 6,
-        damage: {"lightning": "1d8"},
+        tohit: 7,
+        damage: {"lightning": "2d8"},
         secondary: {"desc": "target cannot use reaction this turn"},
         sounds: SPELL_SOUNDS,
       }, {
         name: "Chromatic Orb",
-        tohit: 6,
+        tohit: 7,
         damage: {"acid|cold|fire|lightning|poison|thunder": "3d8"},
         conditions: {
           "level 2": {"weapon": "d8"},
@@ -29,7 +29,7 @@ const party_data = {
         sounds: SPELL_SOUNDS,
       }, {
         name: "Burning Hands",
-        save: "14 Dex",
+        save: "15 Dex",
         damage: {"fire": "3d6"},
         conditions: {
           "level 2": {"weapon": "d6"},
@@ -101,7 +101,7 @@ const party_data = {
     attacks: [
       {
         name: "Shortbow",
-        tohit: 8,
+        tohit: 9,
         damage: {"piercing": "1d8+4"},
         conditions: {
           "Hunters Mark": {"weapon": "d6"},
@@ -109,7 +109,7 @@ const party_data = {
         sounds: RANGED_SOUNDS,
       }, {
         name: "Rapier",
-        tohit: 6,
+        tohit: 7,
         damage: {"piercing": "1d8+4"},
         conditions: {
           "Hunters Mark": {"weapon": "d6"},
@@ -121,21 +121,21 @@ const party_data = {
     attacks: [
       {
         name: "Toll the Dead",
-        save: "14 Wis",
+        save: "15 Wis",
         half: false,
-        damage: {"necrotic": "d8"},
+        damage: {"necrotic": "2d8"},
         conditions: {
-          "Injured": {weapon: "d12", replace: true},
+          "Injured": {weapon: "2d12", replace: true},
         },
         sounds: SPELL_SOUNDS,
       }, {
         name: "Spiritual Weapon",
-        tohit: 5,
+        tohit: 7,
         damage: {force: "d8+4"},
         sounds: SPELL_SOUNDS,
       }, {
         name: "Guided Bolt",
-        tohit: 5,
+        tohit: 7,
         damage: {radiant: "4d6", effect: "Next attack on target has advantage"},
         conditions: {
           "level 2": {weapon: "d6"},
@@ -149,14 +149,14 @@ const party_data = {
     attacks: [
       {
         name: "Greataxe",
-        tohit: 6,
+        tohit: 7,
         damage: {"slashing": "2d6+4"},
         conditions: {
           "Raging": {weapon: "2"},
         },
       }, {
         name: "Javelin",
-        tohit: 6,
+        tohit: 7,
         damage: {"piercing": "1d6+4"},
         conditions: {
           "Raging": {weapon: "2"},
@@ -164,7 +164,7 @@ const party_data = {
         sounds: RANGED_SOUNDS,
       }, {
         name: "Light Hammer",
-        tohit: 6,
+        tohit: 7,
         damage: {"piercing": "1d4+4"},
         conditions: {
           "Raging": {weapon: "2"},
@@ -176,7 +176,7 @@ const party_data = {
     attacks: [
       {
         name: "Shadowmourne",
-        tohit: 7,
+        tohit: 8,
         damage: {slashing: "1d10+5"},
         conditions: {
           "Hexed": {weapon: "d6"},
@@ -185,7 +185,7 @@ const party_data = {
         },
       }, {
         name: "Eldritch Blast",
-        tohit: 6,
+        tohit: 7,
         damage: {force: "d10"},
         conditions: {
           "Hexed": {weapon: "d6"},
@@ -199,18 +199,18 @@ const party_data = {
     attacks: [
       {
         name: "Firebolt",
-        tohit: 6,
-        damage: {"fire": "1d10"},
+        tohit: 7,
+        damage: {"fire": "2d10"},
         sounds: SPELL_SOUNDS,
       }, {
         name: "Mind Sliver",
-        save: "14 Int",
+        save: "15 Int",
         half: false,
-        damage: {psychic: "1d6", effect: "Subract 1d4 from next saving throw"},
+        damage: {psychic: "2d6", effect: "Subract 1d4 from next saving throw"},
         sounds: SPELL_SOUNDS,
       }, {
         name: "Dissonant Whispers",
-        save: "14 Wis",
+        save: "15 Wis",
         damage: {psychic: "3d6", effect: "Must use reaction to as far as possible away from you"},
         conditions: {
           "level 2": {"weapon": "1d6"},
@@ -219,7 +219,7 @@ const party_data = {
         sounds: SPELL_SOUNDS,
       }, {
         name: "Arms of Hadar",
-        save: "14 Str",
+        save: "15 Str",
         damage: {necrotic: "2d6", effect: "Cannot take reactions till next turn"},
         conditions: {
           "level 2": {"weapon": "1d6"},
